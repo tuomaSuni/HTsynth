fc9f55bcd2a021b74926d9ff4c3c192544e4769f
tuomaSuni <tuomas.suni@mail.com>
2024-08-15 09:21:09 +0300

    Set up
    * Implemented Hand Tracking in OpenCV and Mediapipe
    * Implemented Data transfer between Python application and Unity Editor utilizing UDP and Sockets
    * Projecting the Datapoints into Unity Scene

---
+++ Assets/Scripts/PointTracker.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTracker : MonoBehaviour
+{
+    public UDPReceiver udpReceiver;
+    public GameObject[] _Point;    // Array to hold the GameObject elements
+
+    void Update()
+    {
+        ProcessData();
+    }
+
+    private void ProcessData()
+    {
+        string data = udpReceiver.data;
+
+        if (!string.IsNullOrEmpty(data) && data.Length > 2)
+        {
+            // Remove the square brackets at the beginning and end
+            data = data.Trim('[', ']');
+
+            // Split the data into tuples
+            string[] tupleStrings = data.Split(new string[] { "), (" }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Process the tuples
+            for (int i = 0; i < tupleStrings.Length && i < _Point.Length; i++)
+            {
+                // Split each tuple into individual coordinates
+                string tuple = tupleStrings[i].Trim('(', ')');
+                string[] coordinates = tuple.Split(',');
+
+                if (coordinates.Length == 3)
+                {
+                    // Parse the x, y, z coordinates and scale as needed
+                    float x = 10 - float.Parse(coordinates[0]) / 100f;
+                    float y =      float.Parse(coordinates[1]) / 100f;
+                    float z =      float.Parse(coordinates[2]) / 100f;
+
+                    Vector3 xyz = new Vector3(x, y, z);
+
+                    // Update the GameObject position
+                    if (_Point[i] != null)
+                    {
+                        _Point[i].transform.position = xyz;
+                    }
+                }
+            }
+        }
+    }
+}
